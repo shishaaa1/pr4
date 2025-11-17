1d97c6244f593b422f097d1142c999c36eb068bb
DanilGashev <lalalamistikc31@gmail.com>
2025-11-18 00:22:08 +0500

    Добавьте файлы проекта.

---
+++ Common/ViewModelSend.cs
@@ -0,0 +1,13 @@
+﻿namespace Common
+{
+    public class ViewModelSend
+    {
+        public string Message { get; set; }
+        public int Id {  get; set; }
+        public ViewModelSend(string message, int Id)
+        {
+            Message = message;
+            this.Id = Id;
+        }
+    }
+}
