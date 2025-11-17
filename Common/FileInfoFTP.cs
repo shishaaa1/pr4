8c9c10839b013235cd7c9e54c0cc68375bda58be
DanilGashev <lalalamistikc31@gmail.com>
2025-11-18 00:24:28 +0500

    Создана библиотека классов Common

---
+++ Common/FileInfoFTP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class FileInfoFTP
+    {
+        public byte[] Data { get; set; }
+        public string Name { get; set; }
+        public FileInfo FTP(byte[] data,string name) {
+            Data = data;
+            Name = name;
+        }
+    }
+}
---
+++ Common/ViewModelMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ViewModelMessage
+    {
+        public string Command { get; set; }
+        public string Data { get; set; }
+        public ViewModelMessage(string command,string data) {
+            Command = command;
+            Data = data;
+        }
+    }
+}
