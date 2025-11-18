fea9bac86d8cfdd7e464f65b9b2e4f68612d5107
danik31-h <filippov31012007@gmail.com>
2025-11-18 14:51:54 +0500

    Прописан метод CheckCommand

---
+++ Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft;
+using Common;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+
+        public static IPAddress IpAdress;
+        public static int Port;
+        public static int Id = -1;
+        public static bool CheckCommand(string message)
+        {
+            bool BCommand = false;
+            string[] DataMessage = message.Split(new string[1] { " " }, StringSplitOptions.None);
+            if (DataMessage.Length > 0)
+            {
+                string Command = DataMessage[0];
+                if (Command == "connect")
+                {
+                    if (DataMessage.Length != 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Использование: connect[login] [password \nПример: connect User1 P@ssw0rd]");
+                        BCommand = true;
+                    }
+                    else
+                        BCommand = true;
+                }
+                else if (Command == "cd")
+                    BCommand = true;
+                else if (Command == "get")
+                {
+                    if (DataMessage.Length == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Использование: get [NameFIle]\nПример:get Test.txt");
+                        BCommand = false;
+                    }
+                    else
+                        BCommand = true;
+                }
+                else if (Command == "set")
+                {
+                    if (DataMessage.Length == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Использование: set [NameFile]\nПример: set Test.txt");
+                        BCommand = false;
+                    }
+                    else
+                        BCommand = true;
+                }
+            }
+            return BCommand;
+        }
+    }
+}
